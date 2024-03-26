b0dc39a52851f77da7e75ff376f4f8a5a11591c4
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-26 16:00:46 +0100

    created type seeder

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Generator $faker): void
+    {
+        $label = ['FrontEnd', 'BackEnd', 'UI/UX', 'Fullstack', 'Design'];
+
+        foreach($labels as $label){
+            $type = new Type();
+
+            $type->label = $label;
+            $type->color = $faker->hexColor();
+
+            $type->save();
+        }
+    }
+}
